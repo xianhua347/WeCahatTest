744df6658ceb07156141ddb9d6a01dafe35a91fe
xianhua347 <3389162133@qq.com>
2022-10-20 17:31:04 +0800

    Initial commit

---
+++ src/main/java/com/example/wecahattest/WeCahatTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.wecahattest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeCahatTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeCahatTestApplication.class, args);
+    }
+
+}
